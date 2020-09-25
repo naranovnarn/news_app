f51ae20cf76430f37ef68a87e7d18d575b91d36a
naranovnarn <narn-n@taxnet.ru>
2020-09-25 14:11:09 +0300

    Updates

---
+++ precache-manifest.fba0855d9e5806a63da14314fed7afcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af342c4c0291799fd5b41de53623bacf",
+    "url": "/news_app/index.html"
+  },
+  {
+    "revision": "e1503bb610a8027db399",
+    "url": "/news_app/static/css/main.7ee4d6ce.chunk.css"
+  },
+  {
+    "revision": "c163dd34e476b04ffa97",
+    "url": "/news_app/static/js/2.b17e1e75.chunk.js"
+  },
+  {
+    "revision": "d96b32f6ed4cb5911efceabe2ea1a2be",
+    "url": "/news_app/static/js/2.b17e1e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1503bb610a8027db399",
+    "url": "/news_app/static/js/main.2e9b6e57.chunk.js"
+  },
+  {
+    "revision": "133750ec4a7ca6dc8193",
+    "url": "/news_app/static/js/runtime-main.68f8492f.js"
+  }
+]);
